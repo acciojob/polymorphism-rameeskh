16240534497556bcabf0cf48af8b466ea342574d
RAMEES K H <khramees.ramees@gmail.com>
2022-10-05 12:26:23 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,31 @@
 package com.driver;
 
 public class Main {
+  public static void main(String[] args) {
+    Product p=new Product();
+    int ans1=p.product(10,20);
+    System.out.println(ans1);
+    int ans2=p.product(10,20,30);
+    System.out.println(ans2);
+    double ans3=p.product(3.2,4.5);
+    System.out.println(ans3);
+}
+static class Product{
+    public Product(){
 
+    }
+    public int product(int x,int y){
+        return x*y;
+    }
+    public int product(int x,int y,int z){
+        return x*y*z;
+    }
+
+    public double product(double x,double y){
+        return  x*y;
+    }
+
+
+}
     
   }
